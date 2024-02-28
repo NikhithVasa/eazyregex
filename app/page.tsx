4d770dc3b096ec8baf7f07d8dcac5fa876080cc4
Nikhith.Vasa <nikhith.vasa1@target.com>
2024-02-28 14:07:13 -0600

    remove

--- app/page.tsx
+++ app/page.tsx
@@ -67,7 +67,6 @@ export default function Home() {
       <div>
         <div>
           <div>
-            <div style={{ fontSize: '1.5em', fontWeight: 'bold' }}>The present representatives currently holding office in Telangana.</div>
             <div
               style={{
                 fontSize: '25px',
